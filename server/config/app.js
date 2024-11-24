28d263f7324b537ab04c63697b18fadaa0e519b9
ScareCrowvv <146904754+ScareCrowvv@users.noreply.github.com>
2024-11-24 03:51:59 -0500

    Add files via upload

--- server/config/app.js
+++ server/config/app.js
@@ -4,56 +4,90 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
-// Create an Express app instance
 let app = express();
 
-// Import routers
+let cors = require('cors')
+
+// Create user model instance
+
+let userModel = require('../model/User');
+let User = userModel.User;
+
 let indexRouter = require('../routes/index');
 let usersRouter = require('../routes/users');
 let movieRouter = require('../routes/movie');
 
-// Set views directory and view engine (EJS)
+
+// view engine setup
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
+let session = require('express-session')
+let passport = require('passport')
+let passportLocal = require('passport-local')
+let flash = require('connect-flash')
 
-// MongoDB connection setup
+passport.use(User.createStrategy());
+let localStrategy = passportLocal.Strategy;
+
+// getting-started.js
 const mongoose = require('mongoose');
 let DB = require('./db');
-mongoose.connect(DB.URI); 
-let mongoDB = mongoose.connection; 
-mongoDB.on('error', console.error.bind(console, 'Connection Error')); 
-mongoDB.once('open', () => { 
-  console.log("Connected with MongoDB");
+// point mongoose to the DB URI
+mongoose.connect(DB.URI);
+let mongoDB = mongoose.connection;
+mongoDB.on('error',console.error.bind(console,'Connection Error'));
+mongoDB.once('open',()=>{
+  console.log("Connected with the MongoDB")
 });
+mongoose.connect(DB.URI,{useNewURIParser:true,useUnifiedTopology:true})
 
-// Additional mongoose settings
-mongoose.connect(DB.URI, { useNewUrlParser: true, useUnifiedTopology: true }); // Mongoose connection options
+// set up express-session
+app.use(session({
+  secret:"SomeSecret",
+  saveUninitialized: false,
+  resave: false
+}))
 
-// Use middleware for logging, parsing requests, and serving static files
-app.use(logger('dev'));  
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: false })); 
-app.use(cookieParser()); 
-app.use(express.static(path.join(__dirname, '../../public'))); 
-app.use(express.static(path.join(__dirname, '../../node_modules'))); 
+// initialize the flash
 
-// Set up routes
-app.use('/', indexRouter);  
-app.use('/users', usersRouter);  
-app.use('/movieslist', movieRouter);  
-// Handle 404 errors for undefined routes
+app.use(flash());
+
+// serialize and deserialize the user information
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(__dirname, '../../node_modules')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/movieslist',movieRouter);
+// /project --> projectrouter
+// /contactus --> contactus
+
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404)); 
+  next(createError(404));
 });
 
-// Error handler
+// error handler
 app.use(function(err, req, res, next) {
-  res.locals.message = err.message; 
-  res.locals.error = req.app.get('env') === 'development' ? err : {}; 
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  res.status(err.status || 500); 
-  res.render('error', { title: 'Error' }); e
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error',{title:'Error'});
 });
 
-// Export the app module to use in other files
 module.exports = app;
--- server/routes/movie.js
+++ server/routes/movie.js
@@ -1,73 +1,132 @@
 var express = require('express');
 var router = express.Router();
-let mongoose = require('mongoose');
-let Movie = require('../model/movie');
+let mongoose=require('mongoose');
+//Telling my router that i have this model
+let Movie = require("../model/movie")
+const movie=require("../model/movie")
+let movieController = require('../controllers/movie.js')
+function requireAuth(req,res,next)
+{
+    if (!req.isAuthenticated())
+    {
+        return res.redirect('/login');
+    }
+    next();
+}
+ 
 
-// List all movies
-router.get('/', async (req, res, next) => {
-    try {
-        const MovieList = await Movie.find();
-        res.render('Movie/list', { title: 'Movies', MovieList });
-    } catch (err) {
+router.get('/',async(req,res,next)=>{
+try{
+    const MovieList= await Movie.find();
+    res.render('Movie/list', {
+        title:'Movies',
+        displayName: req.user?req.user.displayName:'',
+        MovieList:MovieList
+    })}
+    catch(err){
         console.error(err);
-        res.render('Movie/list', { error: 'Error on the server' });
+        res.render('Movie/list',{
+            error:'Error on the server'
+        })
+    }
+    });
+
+router.get('/add',async(req,res,next)=>{
+    try{
+        res.render('Movie/add',{
+            title: 'Add Movie',
+            displayName: req.user?req.user.displayName:'' 
+        })
+
+    }
+    catch(err)
+    {
+        console.error(err);
+        res.render('Movie/list',{
+            error:'Error on the server'
+        })
     }
 });
 
-// Show add movie form
-router.get('/add', async (req, res, next) => {
-    try {
-        res.render('Movie/add', { title: 'Add Movie' });
-    } catch (err) {
+
+
+router.post('/add',async(req,res,next)=>{
+    try{
+        let newMovie= Movie ({
+            "Title":req.body.Title,
+            "Genre":req.body.Genre,
+            "ReleaseYear":req.body.ReleaseYear,
+            "Rating":req.body.Rating,
+            "Description":req.body.Description,
+            "Status":req.body.Status
+        });
+        Movie.create(newMovie).then(()=>{
+            res.redirect('/movieslist');
+        })
+    }
+    catch(err)
+        {
+            console.error(err);
+            res.render('/movieslist',{
+                error:'Error on the server'
+            })
+
+        }
+    })
+
+router.get('/edit/:id',async(req,res,next)=>{
+    try{
+        const id=req.params.id;
+        const movieToEdit=await Movie.findById(id);
+        res.render('Movie/edit',
+            {
+                title:'Edit Movie',
+                displayName: req.user?req.user.displayName:'',
+                Movie:movieToEdit
+            }
+        )
+    }
+    catch(err)
+    {
         console.error(err);
-        res.render('Movie/list', { error: 'Error on the server' });
+        next(err); 
     }
 });
-
-// Add new movie
-router.post('/add', async (req, res, next) => {
-    try {
-        let newMovie = new Movie(req.body);  // Create movie from form data
-        await Movie.create(newMovie);
-        res.redirect('/movieslist');
-    } catch (err) {
+router.post('/edit/:id',async(req,res,next)=>{
+    try{
+        let id=req.params.id;
+        let updatedMovie = Movie({
+            "_id":id,
+            "Title":req.body.Title,
+            "Genre":req.body.Genre,
+            "ReleaseYear":req.body.ReleaseYear,
+            "Rating":req.body.Rating,
+            "Description":req.body.Description,
+            "Status":req.body.Status
+        })
+        Movie.findByIdAndUpdate (id,updatedMovie).then(()=>{
+            res.redirect('/movieslist')
+        })
+    }
+    catch(err){
         console.error(err);
-        res.render('Movie/list', { error: 'Error on the server' });
+        res.render('/movieslist',{
+            error:'Error on the server'
+        })
     }
 });
-
-// Show movie edit form
-router.get('/edit/:id', async (req, res, next) => {
-    try {
-        const movieToEdit = await Movie.findById(req.params.id);
-        res.render('Movie/edit', { title: 'Edit Movie', Movie: movieToEdit });
-    } catch (err) {
+router.get('/delete/:id',async(req,res,next)=>{
+    try{
+        let id=req.params.id;
+        Movie.deleteOne({_id:id}).then(()=>{
+            res.redirect('/movieslist')
+        })
+    }
+    catch(error){
         console.error(err);
-        next(err);
+        res.render('/movieslist',{
+            error:'Error on the server'
+        })
     }
 });
-
-// Update movie
-router.post('/edit/:id', async (req, res, next) => {
-    try {
-        let updatedMovie = { ...req.body, _id: req.params.id };
-        await Movie.findByIdAndUpdate(req.params.id, updatedMovie);
-        res.redirect('/movieslist');
-    } catch (err) {
-        console.error(err);
-        res.render('Movie/list', { error: 'Error on the server' });
-    }
-});
-
-// Delete movie
-router.get('/delete/:id', async (req, res, next) => {
-    try {
-        await Movie.deleteOne({ _id: req.params.id });
-        res.redirect('/movieslist');
-    } catch (err) {
-        console.error(err);
-        res.render('Movie/list', { error: 'Error on the server' });
-    }
-});
-
-module.exports = router;
+module.exports=router;
